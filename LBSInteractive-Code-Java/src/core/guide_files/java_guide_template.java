1a5eec793b8527ce6a8cb6d351264182c8842819
LBSInteractive <36526361+LBSInteractive@users.noreply.github.com>
2018-02-25 18:27:43 -0500

    Documentos Files + IDE Eclipse files
    Ubicar esto archivos en la carpeta de proyecto, en el WorkSpace que asignamos en eclipse

---
+++ LBSInteractive-Code-Java/src/core/guide_files/java_guide_template.java
@@ -0,0 +1,82 @@
+package core.guide_files;
+
+//import java.util.ArrayList;
+
+public class java_guide_template {
+	
+	/*Orden de evaluación en operadores de igualdades
+	 * 1. !
+	 * 2. &&
+	 * 3. ||
+	 * */
+	
+	/*Orden algebraico
+	 * 1. Parentesis y llaves
+	 * 2. Potencias y raices
+	 * 3. Multiplicaciones y diviciones 
+	 * 4. Sumas y restas 
+	*/
+	
+	/*If rapido 
+	 * (variable1>variable2) ? valorParaSi:varlorParaNo;
+	 * Ejemplo
+	 * String Variable = (1>0) ? "Si":"No";
+	 * ahora la Variable String seria Si 
+	 */
+	
+	/*Necesita importar java.util.ArrayList;
+	 * ArrayList<String> miArray =new ArrayList<String>();
+	 * Colecciones de un mismo tipo de dato.
+	 * 
+	 * agregar datos al ArrayList
+	 * miArray.add(valor);
+	 * miArray.add(index,valor);
+	 * 
+	 * obtener valor de ArrayList
+	 * miArray.get(index);
+	 * 
+	 * 
+	 * Tamaño de ArrayList
+	 * miArray.size();
+	 */
+	
+	/*Ciclo for each
+	 * for(Integer valor:Array){
+	 * 	System.out.println(valor);
+	 * }
+	 */
+	
+	/*HashMap usado como array indexado
+	 * HashMap<String, Integer> myHas = new HashMap<String, Integer>();
+	 * 
+	 * Agregar datos al HashMap
+	 * myHas.put("valorString",valorInt);
+	 * 
+	 * 
+	 * Obtener dato del HashMap
+	 * myHas.get(IndexString);
+	 * 
+	 * 
+	 * Obtener tamano del HashMap
+	 * myHas.size();
+	 * 
+	 * 
+	 * Obtener cada key de HashMap
+	 * myHas.keySet();
+	 * 
+	 * 
+	 * for para HashMap
+	 * for(String valor:HashMap.keyset()){
+	 * myHas.get(valor);
+	 * }
+	 * 
+	 * 
+	 */
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
